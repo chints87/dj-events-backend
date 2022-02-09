a146a2eb249343302c977f956135e5905c06ed00
Chintan <cgandhi4@gmail.com>
2022-02-09 16:54:53 +0530

    Add Event model

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6b38c23aff013e889dc45c95f748cb36'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '97571717-1cf8-4753-9f78-6ce17c4b21d5'
+};
