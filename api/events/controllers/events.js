a307b550ad5edb66377ae0cda50c33b9551eea42
Chintan <cgandhi4@gmail.com>
2022-02-14 20:13:34 +0530

    Add function to capture logged in user's comment

--- api/events/controllers/events.js
+++ api/events/controllers/events.js
@@ -1,8 +1,31 @@
 'use strict';
-
+const { sanitizeEntity } = require('strapi-utils')
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+// Over-ride existing default controllers or create new ones
+
+module.exports = {
+    // Get logged in users events
+    async me(ctx){
+        // Gets user info from the JSON web token
+        const user = ctx.state.user;
+
+        if(!user){
+            return ctx.badRequest(null, 
+                [{messages: [{id: "No authorization header was found"} ]}])
+        }
+        
+        // Get event data of the user from the DB
+        const data = await strapi.services.events.find({user: user.id})
+
+        if(!data){
+            return ctx.notFound()
+        }
+        
+        // Return data in the object form 
+        return sanitizeEntity(data, { model: strapi.models.events})
+    }
+};
